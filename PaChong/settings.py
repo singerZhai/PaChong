247c5c4ded97a5133430fee9db61b0f41347ffbd
v_zhaihuide <singer_zhai@163.com>
2019-06-08 16:01:05 +0800

    转移项目

--- PaChong/settings.py
+++ PaChong/settings.py
@@ -22,7 +22,7 @@ USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
